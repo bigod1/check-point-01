44f1ee4b0f13e3eb2fb2d6ae84f48bceeeb9bf7f
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-29 10:35:48 -0300

    add: exercicio 1 completo

---
+++ script.js
@@ -0,0 +1,38 @@
+const nome = "Luis Alberto Rocha Filho"
+const idade = 25
+const status = true
+const valorProduto = 10.50
+const times = ["Losangeles Lakers", "Boston Celtics", "Miami Heat"]
+
+const listaVariaveis = [nome, idade, status, valorProduto, times]
+
+console.log(nome)
+console.log(idade)
+console.log(status)
+console.log(valorProduto)
+console.log(times)
+
+function soma (valor1, valor2){
+    const resultado = valor1 + valor2
+    console.log(resultado)
+}
+soma(10, 5)
+
+function subtracao (valor1, valor2){
+    const resultado = valor1 - valor2
+    console.log(resultado)
+}
+subtracao(10, 5)
+
+function multiplicacao (valor1, valor2){
+    const resultado = valor1 * valor2
+    console.log(resultado)
+}
+multiplicacao(10, 5)
+
+function divisao (valor1, valor2){
+    const resultado = valor1 / valor2
+    console.log(resultado)
+}
+divisao(10, 5)
+
